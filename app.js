e87a5652539fee86351e18acd4c06b771e523cc8
xuziwei <xuziwei89@gmail.com>
2020-01-30 12:41:18 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById("book-list");
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+  list.appendChild(row);
+};
+
+// Event listener
+document.getElementById("book-form").addEventListener("submit", function(e) {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+
+  const book = new Book(title, author, isbn);
+  const ui = new UI();
+
+  ui.addBookToList(book);
+
+  e.preventDefault();
+});
